d5ce8efeb50e19936874cbe48380e236bc8d69b0
MADHAV <madhavmahajan152@gmail.com>
2025-08-29 03:21:34 +0530

    deployment

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
+app.set('view engine', 'ejs'); // Add this line to set the view engine
 
 app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
 app.get("/",(req,res)=>{
     res.render("index.ejs");
-  })
+})
